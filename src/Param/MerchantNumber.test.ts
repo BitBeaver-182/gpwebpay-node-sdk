d969b197e1810fb81c2cdc40dfaf7b3e8857b2e4
fedediiaz <federico.n.diaz92@gmail.com>
2025-05-15 20:21:45 +0200

    Create MerchantNumber.test.ts

---
+++ src/Param/MerchantNumber.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest';
+import { MerchantNumber } from './MerchantNumber';
+import { Param } from '../Enum/Param';
+import { InvalidArgumentException } from '../Exceptions/InvalidArgumentException';
+import { faker } from '@faker-js/faker';
+
+describe('MerchantNumber', () => {
+  it('should create a MerchantNumber instance correctly', () => {
+    const merchantNumber = new MerchantNumber('FA12345678');
+
+    expect(String(merchantNumber)).toBe('FA12345678');
+    expect(merchantNumber.getValue()).toBe('FA12345678');
+    expect(merchantNumber.getParamName()).toBe(Param.MERCHANTNUMBER);
+  });
+
+  it('should throw InvalidArgumentException for a long merchant number', () => {
+    const invalidString = faker.string.alphanumeric({ length: 300 })
+    expect(() => new MerchantNumber(invalidString)).toThrowError(InvalidArgumentException);
+    expect(() => new MerchantNumber(invalidString)).toThrowError(`MERCHANTNUMBER max. length is 10! "${invalidString.length}" given.`);
+  });
+});
