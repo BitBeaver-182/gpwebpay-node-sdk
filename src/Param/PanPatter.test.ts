1a493b035beac80d654b7af1287854d1e7ba6b0e
fedediiaz <federico.n.diaz92@gmail.com>
2025-05-15 20:31:53 +0200

    Create PanPatter.test.ts

---
+++ src/Param/PanPatter.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import { Param } from '../Enum/Param';
+import { InvalidArgumentException } from '../Exceptions/InvalidArgumentException';
+import { PanPattern } from './PanPattern';
+import { faker } from '@faker-js/faker';
+
+describe('PanPattern', () => {
+  it('should create a PanPattern instance correctly', () => {
+    const panPattern = new PanPattern('panPattern');
+
+    expect(String(panPattern)).toBe('panPattern');
+    expect(panPattern.getValue()).toBe('panPattern');
+    expect(panPattern.getParamName()).toBe(Param.PANPATTERN);
+  });
+
+  it('should throw InvalidArgumentException for a long pan pattern', () => {
+    const invalidText = faker.string.alphanumeric({ length: 256 })
+    expect(() => new PanPattern(invalidText)).toThrowError(InvalidArgumentException);
+    expect(() => new PanPattern(invalidText)).toThrowError(
+      `PANPATTERN max. length is 255! "${invalidText.length}" given.`
+    );
+  });
+});
