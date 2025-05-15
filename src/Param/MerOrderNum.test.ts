ad803a5d6a4bf4efd915d072ab984b046e42fe87
fedediiaz <federico.n.diaz92@gmail.com>
2025-05-15 20:16:02 +0200

    Create MerOrderNum.test.ts

---
+++ src/Param/MerOrderNum.test.ts
@@ -0,0 +1,50 @@
+import { describe, test, expect } from 'vitest';
+import { Param } from '../Enum/Param';
+import { InvalidArgumentException } from '../Exceptions/InvalidArgumentException';
+import { MerOrderNum } from './MerOrderNum';
+import { faker } from '@faker-js/faker';
+
+/**
+ * Test for MerOrderNum parameter
+ * @author  Ondra Votava <ondra@votava.dev>
+ */
+describe('MerOrderNum', () => {
+  test('should successfully create MerOrderNum param', () => {
+    const merOrderNum = new MerOrderNum(123455);
+    expect(merOrderNum.getValue()).toBe(123455);
+    expect(String(merOrderNum)).toBe('123455');
+    expect(merOrderNum.getParamName()).toBe(Param.MERORDERNUM);
+  });
+
+  test.each([
+    ['1.0'],
+    [10.01]
+  ])('should throw exception for invalid value %s', (value) => {
+    expect(() => {
+      new MerOrderNum(value);
+    }).toThrow(InvalidArgumentException);
+    expect(() => {
+      new MerOrderNum(value);
+    }).toThrow(`MERORDERNUM must be integer "${value}" given.`);
+  });
+
+  test('should throw exception for too long value', () => {
+    const longValue = faker.string.numeric({ length: 31 })
+    expect(() => {
+      new MerOrderNum(longValue);
+    }).toThrow(InvalidArgumentException);
+    expect(() => {
+      new MerOrderNum(longValue);
+    }).toThrow(`MERORDERNUM max. length is 30! "${longValue.length}" given.`);
+  });
+
+  test('should throw exception for invalid scalar value', () => {
+    const invalidString = faker.string.alphanumeric({ length: 4 })
+    expect(() => {
+      new MerOrderNum(invalidString);
+    }).toThrow(InvalidArgumentException);
+    expect(() => {
+      new MerOrderNum(invalidString);
+    }).toThrow(`MERORDERNUM must be integer "${invalidString}" given.`);
+  });
+});
