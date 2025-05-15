99005b9598201a00e35a1d1c7da74d599f8e169c
fedediiaz <federico.n.diaz92@gmail.com>
2025-05-15 19:32:59 +0200

    Create AddInfo test

---
+++ src/Param/AddInfo.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest';
+import { AddInfo } from './AddInfo';
+import { InvalidArgumentException } from '../Exceptions/InvalidArgumentException';
+import { faker } from '@faker-js/faker';
+
+describe('AddInfo', () => {
+  it('should create successfully with valid XML string', () => {
+    const xml = '<xml></xml>';
+    const addInfo = new AddInfo(xml);
+
+    expect(addInfo.getValue()).toBe(xml);
+    expect(String(addInfo)).toBe(xml);
+  });
+
+  it('should throw InvalidArgumentException if string exceeds 24000 characters', () => {
+    const xml = '<xml>' + faker.string.alphanumeric({ length: 30000 }) + '</xml>';
+    const expectedLength = xml.length;
+
+    expect(() => new AddInfo(xml)).toThrowError(
+      new InvalidArgumentException(`ADDINFO max. length is 24000! "${expectedLength}" given.`)
+    );
+  });
+});
