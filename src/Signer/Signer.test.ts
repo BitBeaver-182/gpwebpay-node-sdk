96d2bcb13a816a5086c6865eb9c3da6a9f96c2a1
fedediiaz <federico.n.diaz92@gmail.com>
2025-05-17 00:27:27 +0200

    Create Signer.test.ts

---
+++ src/Signer/Signer.test.ts
@@ -0,0 +1,59 @@
+import { describe, it, expect } from 'vitest';
+import path from 'path';
+import { PrivateKey } from './Key/PrivateKey';
+import { PublicKey } from './Key/PublicKey';
+import { Signer } from './Signer';
+
+const privateKeyPath = path.join(__dirname, '../../tests/_certs/test.pem');
+const publicKeyPath = path.join(__dirname, '../../tests/_certs/test-pub.pem');
+
+const params = {
+  MERCHANTNUMBER: '123456789',
+  DEPOSITFLAG: '1',
+  OPERATION: 'CREATE_ORDER',
+  AMOUNT: '100000',
+  ORDERNUMBER: '123456',
+  CURRENCY: '203',
+  MD: 'czk',
+  URL: 'http://test.com',
+};
+
+describe('Signer', () => {
+  it('signs data correctly', () => {
+    const signer = new Signer(
+      new PrivateKey(privateKeyPath, '1234567'),
+      new PublicKey(publicKeyPath)
+    );
+
+    const hash = signer.sign(params);
+
+    expect(hash).toBe(
+      'kMl9tg/up2z9CJu+Ebgm7mg3XSGOAvY2ZkrtgqOtzSprh1L22bvshGRlfDT9134Z2Hj1PWNitDOvgoAFnxyax8oIyx6eB4hMnNkB6xyr3X5XQXqsCsVRGYHYUOLvNuAag1kaNcVx+'
+      + 'juqijxd0huvk60PMn5JjQijNl4ij36YwoqyN4UdP16LjIqYRIngaeHsTTR1XgIVmJIcuIfETV1QsiQCOYPw0s/ZTeri1DzpQq1Es5cERSupFBVp5Y8tJUna0Yx/oLh2SBhsw6BPixm6jhLAj'
+      + 'qvQn+gmMv4AKDfTYdSPDqg1A+T3XFK/+vE+zOGW0/DHKr2ZqNYUQyD1adi3QA=='
+    );
+  });
+
+  it('verifies valid signature successfully', () => {
+    const signer = new Signer(
+      new PrivateKey(privateKeyPath, '1234567'),
+      new PublicKey(publicKeyPath)
+    );
+
+    const hash = signer.sign(params);
+    const result = signer.verify(params, hash);
+
+    expect(result).toBe(true);
+  });
+
+  it('fails verification with bad hash', () => {
+    const signer = new Signer(
+      new PrivateKey(privateKeyPath, '1234567'),
+      new PublicKey(publicKeyPath)
+    );
+
+    const result = signer.verify(params, 'badhash');
+
+    expect(result).toBe(false);
+  });
+});
