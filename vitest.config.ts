8fc543a37cda3202785e57040be0a2586775a09b
fedediiaz <federico.n.diaz92@gmail.com>
2025-05-17 22:10:04 +0200

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,22 @@
-// vitest.config.ts
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths'; // Or @rollup/plugin-alias
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      provider: 'v8', // or 'istanbul'
+      reporter: ['text', 'json-summary', 'json', 'html'],
+      thresholds: {
+        lines: 96,
+        branches: 95,
+        functions: 95,
+        statements: 95,
+      },
+    },
+
+  },
   plugins: [tsconfigPaths()],
 });
